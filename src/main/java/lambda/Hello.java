94b6ae99707b8464066ee0873c0a18f4c31ff5b7
Anish Sana <anishsana91@gmail.com>
2017-07-01 16:51:17 -0400

    Initial commit for HelloFunction

---
+++ src/main/java/lambda/Hello.java
@@ -0,0 +1,13 @@
+package lambda;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Hello implements RequestHandler<String, String> {
+
+	@Override
+	public String handleRequest(String input, Context context) {
+		String output = "Hello, " + input + "!";
+		return output;
+	}
+
+}
